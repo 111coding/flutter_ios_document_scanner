75a8a3669ae9445bb9361e01b6d931b089d41176
mark <mark@markui-MacBookPro.local>
2022-04-19 11:46:16 +0900

    OnlyIos Process

--- lib/flutter_ios_document_scanner.dart
+++ lib/flutter_ios_document_scanner.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
@@ -6,6 +7,7 @@ class FlutterIosDocumentScanner {
   static const MethodChannel _channel = MethodChannel('flutter_ios_document_scanner');
 
   static Future<bool?> scanDocument() async {
+    if (!Platform.isIOS) return null;
     return await _channel.invokeMethod('scan_document');
   }
 }
